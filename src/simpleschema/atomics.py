7733b30c88ad78a3a18b88e26830499083a3ce64
lea <leaprovenzano@gmail.com>
2019-10-07 09:17:55 +0100

    move SchemaType to types & fix inheretence on atomics

--- src/simpleschema/atomics.py
+++ src/simpleschema/atomics.py
@@ -1,20 +1,5 @@
-from abc import abstractclassmethod, ABCMeta
-
 from simpleschema.schema import BooleanSchema, IntegerSchema, NumberSchema, NullSchema, StringSchema
-
-
-class SchemaType(metaclass=ABCMeta):
-
-    __schema_cls__ = NotImplemented
-
-    @abstractclassmethod
-    def __schema__(cls, **kwargs):
-        return NotImplemented
-
-    def __subclasshook__(cls, t: type) -> bool:
-        if cls is SchemaType:
-            return hasattr(t.__schema__) and callable(t.__schema__)
-        return NotImplemented
+from simpleschema.types import SchemaType
 
 
 class AtomicSchema(SchemaType):
@@ -22,27 +7,26 @@ class AtomicSchema(SchemaType):
     __schema_cls__ = NotImplemented
     __atomic__ = True
 
-    @abstractclassmethod
     def __schema__(cls, **kwargs):
         return cls.__schema_cls__(**kwargs).to_dict()
 
 
-class String(SchemaType, str):
+class String(AtomicSchema, str):
 
     __schema_cls__ = StringSchema
 
 
-class Number(SchemaType, float):
+class Number(AtomicSchema, float):
 
     __schema_cls__ = NumberSchema
 
 
-class Integer(SchemaType, int):
+class Integer(AtomicSchema, int):
 
     __schema_cls__ = IntegerSchema
 
 
-class Boolean(SchemaType):
+class Boolean(AtomicSchema):
 
     __supertype__ = bool
     __schema_cls__ = BooleanSchema
@@ -51,7 +35,7 @@ class Boolean(SchemaType):
         return bool(x)
 
 
-class Null(SchemaType):
+class Null(AtomicSchema):
 
     __supertype__ = type(None)
     _schema_cls = NullSchema
